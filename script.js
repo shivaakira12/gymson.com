17aa79af9469563f7edb6e1b7b305aa8b90e81dd
shivaakira12 <shivaakira000@gmail.com>
2021-11-28 14:35:55 +0530

    ss

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ var swiper = new Swiper(".home-slider", {
 	  clickable: true,
 	},
 });
+function bmi(){
+	var height=document.getElementById('height').value;
+	var weight=document.getElementById('weight').value;
+	var bmi=weight/(height/100*height/100);
+	var total=bmi.toFixed(2);
+	document.getElementById('result').innerHTML="Your BMI is "+total;
+	if (height === "" || isNaN(height)) 
+	result.innerHTML = "Provide a valid Height!";
+
+	else if (weight === "" || isNaN(weight)) 
+	result.innerHTML = "Provide a valid Weight!";
+
+	else{
+		if (bmi < 18.6) result.innerHTML =
+		`Under Weight : <span>${bmi}</span>`;
+
+	else if (bmi >= 18.6 && bmi < 24.9) 
+		result.innerHTML = 
+			`Normal : <span>${bmi}</span>`;
+
+	else result.innerHTML =
+		`Over Weight : <span>${bmi}</span>`;
+	}
+
+}
